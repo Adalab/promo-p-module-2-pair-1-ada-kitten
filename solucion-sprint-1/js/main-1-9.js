af5c65cc94f0f5088ba91420d1ce6343c5ef386f
Veronica isla <veroislagleaniz@gmail.com>
2022-03-31 11:33:53 +0200

    create new cat

---
+++ solucion-sprint-1/js/main-1-9.js
@@ -0,0 +1,21 @@
+// 2. Agregar un nuevo gatito al listado
+// Hasta el momento en la aplicación trabajamos con tres gatitos, en este ejercicio vamos a agregar desde el formulario nuevos gatitos al listado. Para ello:
+
+// Escucha el evento asociado al evento click del botón Guardar del formulario.
+
+// Modifica la función manejadora del evento addNewKitten(ev).
+// Dentro de esta función crea un nuevo objeto newKittenDataObject. ¿De dónde sale toda la información que enviamos al servidor? La información la obtenemos desde el formulario de la aplicación, con los valores de cada uno de los inputs. Recuerda que cada gatito tiene una imagen, name, desc y race
+// const newKittenDataObject = {
+//   //completa el código
+// };
+// Agrega al listado de enlaces con la siguiente línea de código:
+// kittenDataList.push(newKittenDataObject);
+// Limpia los valores de los inputs
+// Muestra un mensaje al usuario: "Mola! Un nuevo gatito en Adalab!".
+// labelMesageError.innerHTML = 'Mola! Un nuevo gatito en Adalab!';
+// Vuelve a pintar el listado de gatitos llamando a la función renderKittenList(kittenDataList)
+
+const cats = [
+  katName: ¡
+
+]
